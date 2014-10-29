65517fe3559ebd9bd915ded293e1d513e5b5aa6c
pglotfel <pglotfel@ycp.edu>
2014-10-29 11:35:09 -0400

    Small bug fixes in new test

--- test/networking/new_new_test.clj
+++ test/networking/new_new_test.clj
@@ -10,7 +10,7 @@
   
   (def test-cpu (n/cpu {:requires requires :provides provides :ip ip :neighbors neighbors}))
   
-  (let [rs (if (empty? requires) '() (w/outline :getting [(first requires)] (fn [stream] (s/consume #(println "getting: " %) (s/map identity stream)))))
-        ps (if (empty? provides) '() (w/outline (first provides) [] (fn [] (s/periodically 1000 (fn [] :system-one)))))]
+  (let [rs (if (empty? requires) requires [(w/outline :getting [(first requires)] (fn [stream] (s/consume #(println "getting: " %) (s/map identity stream))))])
+        ps (if (empty? provides) provides [(w/outline (first provides) [] (fn [] (s/periodically 1000 (fn [] :system-one))))])]
     
     (apply w/assemble w/manifold-step w/manifold-connect (concat (:system test-cpu) rs ps))))
