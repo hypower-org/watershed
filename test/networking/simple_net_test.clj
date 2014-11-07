f714e01403ae7b31ce2fe9f4a1961f8d459ebf3b
pglotfel <pglotfel@ycp.edu>
2014-11-07 14:29:44 -0500

    Starting to add resiliency into networking

--- test/networking/simple_net_test.clj
+++ test/networking/simple_net_test.clj
@@ -5,13 +5,19 @@
             [aleph.udp :as udp]
             [manifold.deferred :as d]))
 
-(defn start 
-  [arg] 
-  (if arg 
-    (do
-      (def c (n/physi-client {:host "10.10.10.5"}))
-      (def s @(n/physi-server {} "10.10.10.5" "10.10.10.3"))
-      (def c @c))
-    (def c @(n/physi-client {:host "10.10.10.5"}))))
+(defn -main
+  [ip requires provides]
+  
+  (let [sys (:system (n/cpu {:ip ip :neighbors 2 :requires [requires] :provides [provides]}))]
+    
+    (->>
+      
+      sys
+      
+      (cons (w/outline provides [requires] (fn [stream] (s/map (fn [x] provides) stream))))
+      
+      (cons (w/outline :printing [requires] (fn [stream] (s/consume println (s/map identity stream)))))
+    
+      (apply w/assemble w/manifold-step w/manifold-connect))))
     
 
