f37802edb33042a09940f86c3781580c1607ade5
pglotfel <pglotfel@ycp.edu>
2014-11-12 17:41:03 -0500

    Try sleeping?

--- src/net/physi_server.clj
+++ src/net/physi_server.clj
@@ -319,6 +319,7 @@
                                           (fn [x]                                                                                                    
                                             (when-not (= (type x) B-ary)
                                               (println "data: " x)
+                                              (Thread/sleep 1)
                                               (s/put! client (nippy/freeze x))))  
                                           client)))))
                    
