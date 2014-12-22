a85102f4af48aca6dc7835aec7b4de8ac0f5420a
Patrick Martin <prof.pmartin10@gmail.com>
2014-12-22 08:59:08 -0500

    Removed the cpu-units function from watershed and moved it to physicloud utils.

--- src/watershed/utils.clj
+++ src/watershed/utils.clj
@@ -1,6 +1,6 @@
 (ns watershed.utils
   (:use [clojure.java.shell :only [sh]])
-  (:require [no.disassemble :as d]))
+  #_(:require [no.disassemble :as d]))
 
 (defn | 
   [init & fns] 
@@ -15,10 +15,3 @@
   [start-time]
   "Returns the time passed"
   (/ (double (- (. System (nanoTime)) start-time)) 1000000.0))
-
-(defn ^double cpu-units
-  []
-  (let [^String result (filter identity (map (comp last #(if % (clojure.string/split % #"\s+")) #(re-matches #"CPU.*\d" %)) (clojure.string/split (:out (sh "lscpu")) #"\n")))]
-    
-    (* (read-string (first result)) (read-string (last result)))))
-
