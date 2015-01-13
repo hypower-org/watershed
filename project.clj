ef0b1fdb5e620ebc73719db2c1ddb5702138f8c4
Patrick Martin <prof.pmartin10@gmail.com>
2015-01-13 14:00:52 -0500

    Upgraded to beta7 of manifold.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject hypower-org/watershed "0.1.2"
+(defproject hypower-org/watershed "0.1.3"
   :description "A library for compiling graphs!"
   :url "http://github.com/hypower-org/watershed"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]  
-                 [manifold "0.1.0-beta5"]])
+                 [manifold "0.1.0-beta7"]])
