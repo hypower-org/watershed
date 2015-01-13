b8970f0cd5ceb27ba351d1ac1fd5d1aa0786bd4a
Patrick Martin <prof.pmartin10@gmail.com>
2015-01-13 13:58:09 -0500

    Changed project name for maven/clojars.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject watershed "0.1.2"
+(defproject hypower-org/watershed "0.1.2"
   :description "A library for compiling graphs!"
-  :url "http://github.com/pglotfel/watershed"
+  :url "http://github.com/hypower-org/watershed"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]  
