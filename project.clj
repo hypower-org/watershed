fe866a6e91833de94d803fb824a4650d514c6dae
snelso15 <snelso15@ycp.edu>
2015-02-12 10:45:43 -0500

    Updated manifold dependency to beta10.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject hypower-org/watershed "0.1.4"
+(defproject hypower-org/watershed "0.1.5"
   :description "A library for compiling graphs!"
   :url "http://github.com/hypower-org/watershed"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]  
-                 [manifold "0.1.0-beta7"]])
+                 [manifold "0.1.0-beta10"]])
